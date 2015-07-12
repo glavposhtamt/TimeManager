2275d9b0d9dba96acab84960ec0e2a238e98c3d9
Сергей <glavposhtamt@bk.ru>
2015-07-12 23:00:03 +0300

    malloc

--- header.h
+++ header.h
@@ -1,11 +1,23 @@
 #ifndef HEADER
 #define HEADER
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+#define SIZE 128
 
 struct myTymeInfo {
   struct tm arrTimeinfo[128];
   unsigned short length;
 };
+
+typedef struct myTymeInfo mt;
+
+extern char ** readWriteFile();
+extern  mt * getStruct( char ** bigstr );
+extern int a;
 
 #endif // HEADER
 
--- main.c
+++ main.c
@@ -1,19 +1,18 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <time.h>
 
 #include "header.h"
 
 #define SIZE 128
+/*
+int a = 5;
+char ** readWriteFile() {
 
-int main(){
-    typedef struct myTymeInfo mt;
     char str[SIZE];
     int i = 0, j = 0;
-    char bigstr[SIZE][SIZE];
+
+    char ** bigstr = (char **)malloc(128 * sizeof(char *));
+    for(; i < 128; ++i) bigstr[i] = (char *)malloc(128 * sizeof(char *));
+    i = 0;
     FILE * f;
-    mt myt;
     time_t rawtime;
     struct tm * timeinfo;
 
@@ -27,23 +26,50 @@ int main(){
     fprintf(f, "%02d:%02d %02d-%02d-%04d\t%s\n", timeinfo->tm_hour, timeinfo->tm_min, timeinfo->tm_mday, timeinfo->tm_mon, timeinfo->tm_year + 1900, str);
 
     while (fgets (str, SIZE, f) != NULL)  {
-            for(; str[i] != '\n'; ++i ) bigstr[j][i] = str[i];
+            for(; str[i] != '\n'; ++i )
+                bigstr[j][i] = str[i];
             j++, i = 0;
     }
 
     fclose(f);
+    return bigstr;
+}
 
-    for(i = 0; strlen(bigstr[i]) != 0; ++i) {
-        myt.arrTimeinfo[i].tm_hour = atoi(&bigstr[i][0]);
-        myt.arrTimeinfo[i].tm_min = atoi(&bigstr[i][3]);
-        myt.arrTimeinfo[i].tm_mday = atoi(&bigstr[i][5]);
-        myt.arrTimeinfo[i].tm_mon = atoi(&bigstr[i][9]);
-        myt.arrTimeinfo[i].tm_year = atoi(&bigstr[i][12]) - 1900;
-        myt.length = i + 1;
-    }
+ mt * getStruct( char ** bigstr ) {
+     int i = 0;
+     mt * myt;
+     if((myt = malloc(sizeof(mt)))==NULL) {
+         printf("Ошибка при распределении памяти\n");
+         exit(1);
+      }
 
-    printf("%d\t%d\t%d\n", myt.arrTimeinfo[0].tm_min, myt.arrTimeinfo[1].tm_min, myt.length);
+     for(i = 0; strlen(bigstr[i]) != 0; ++i) {
+         myt->arrTimeinfo[i].tm_hour = atoi(&bigstr[i][0]);
+         myt->arrTimeinfo[i].tm_min = atoi(&bigstr[i][3]);
+         myt->arrTimeinfo[i].tm_mday = atoi(&bigstr[i][5]);
+         myt->arrTimeinfo[i].tm_mon = atoi(&bigstr[i][9]);
+         myt->arrTimeinfo[i].tm_year = atoi(&bigstr[i][12]) - 1900;
+         myt->length = i + 1;
+     }
+     return myt;
+}
 
+*/
+int main(){
+    int i = 0;
+    char ** bigstr;
+    mt * myt;
+    bigstr = readWriteFile();
+
+   myt = getStruct(bigstr);
+
+   printf("%d\t%d\t%d\n", myt->arrTimeinfo[0].tm_min, myt->arrTimeinfo[1].tm_min, myt->length);
+
+/*Освобождаем выделенную память*/
+    for(; i < 128; ++i) free(bigstr[i]);
+    free(bigstr);
+    free(myt);
     return 0;
+
  }
 

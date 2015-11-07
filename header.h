5aa169b04fad4ad4679592324bd895c77c96b642
Sergey <glavposhtamt@bk.ru>
2015-11-07 23:56:40 +0300

    Edit header

--- header.h
+++ header.h
@@ -9,7 +9,7 @@ extern void sqlQuery(sqlite3 * db, const char * sql, int (* callback)(void *, in
 extern void initTables(sqlite3 * db, int (* callback)(void *, int, char **, char **));
 extern void addDoing(char * msg, sqlite3 * db, int (* callback)(void *, int, char **, char **));
 extern void selectAll(sqlite3 * db, int (* callback)(void *, int, char **, char **));
-extern void deleteTask(int id, sqlite3 * db, int (* callback)(void *, int, char **, char **))
+extern void deleteTask(int id, sqlite3 * db, int (* callback)(void *, int, char **, char **));
 
 #endif // HEADER
 

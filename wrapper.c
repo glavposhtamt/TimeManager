a0680df12f450d26829f880fb996901208f3b642
Sergey <glavposhtamt@bk.ru>
2016-01-02 17:31:40 +0300

    Ошибка где-то в цикле -надо проветриться

--- wrapper.c
+++ wrapper.c
@@ -5,29 +5,56 @@ int callback(void * data, int argc, char **argv, char ** azColName){ return 0; }
 void sqlQuery(sqlite3 * db, fC callback, char * fmt, ... ){
     
    va_list ap;
-   int d, rc, count = 0;
-   char c, *s, *zErrMsg = 0, query[256], *sql = fmt;
+   int d, rc, count = 0, i = 0;
+   char c, *s, *zErrMsg = 0, query[256], proxy[265], *sql = fmt;
 
    va_start(ap, fmt);
-   while (*fmt)
+   while (*fmt) {
+       query[i++] = *fmt;
+
        if(*fmt++ == '%') {
            switch (*fmt) {
                case 's':              /* string */
+                   query[i++] = 's';
+                   query[i] = '\0';
+
                    s = va_arg(ap, char *);
-                   sprintf(query, sql, s);
+
+                   sprintf(proxy, query, s);
+
+                   for(i = 0; proxy[i] != '\0'; i++) query[i] = proxy[i];
+                   query[i] = '\0';
                    break;
+
                case 'd':              /* int */
+                   query[i++] = 'd';
+                   query[i] = '\0';
+
                    d = va_arg(ap, int);
-                   sprintf(query, sql, d);
+
+                   sprintf(proxy, query, d);
+
+                   for(i = 0; proxy[i] != '\0'; i++) query[i] = proxy[i];
+                   query[i] = '\0';
                    break;
+
                case 'c':              /* char */
+                   query[i++] = 'c';
+                   query[i] = '\0';
+
                    c = (char) va_arg(ap, int);
-                   sprintf(query, sql, c);
+
+                   sprintf(proxy, query, c);
+
+                   for(i = 0; proxy[i] != '\0'; i++) query[i] = proxy[i];
+                   query[i] = '\0';
                    break;
             }
-            ++count;
-            break;
+
+           ++count;
        }
+   }
+
     
    va_end(ap);
 
@@ -42,33 +69,62 @@ void sqlQuery(sqlite3 * db, fC callback, char * fmt, ... ){
 
 values * selectFromTable(sqlite3 * db, char * fmt, ...){
     va_list ap;
-    int rc, nrows, ncols, d, count = 0;
-    char ** result, * errmsg, c, *s, query[256], *sql = fmt;
+    int rc, nrows, ncols, d, count = 0, i = 0;
+    char ** result, * errmsg, c, *s, query[256], proxy[265], *sql = fmt;
     values * val = malloc(sizeof(values));
     
     va_start(ap, fmt);
-    while (*fmt)
-       if(*fmt++ == '%') {
-           switch (*fmt) {
-               case 's':              /* string */
-                   s = va_arg(ap, char *);
-                   sprintf(query, sql, s);
-                   break;
-               case 'd':              /* int */
-                   d = va_arg(ap, int);
-                   sprintf(query, sql, d);
-                   break;
-               case 'c':              /* char */
-                   c = (char) va_arg(ap, int);
-                   sprintf(query, sql, c);
-                   break;
-            }
+    while (*fmt) {
+        query[i++] = *fmt;
+
+        if(*fmt++ == '%') {
+            switch (*fmt) {
+                case 's':              /* string */
+                    query[i++] = 's';
+                    query[i] = '\0';
+
+                    s = va_arg(ap, char *);
+
+                    sprintf(proxy, query, s);
+
+                    for(i = 0; proxy[i] != '\0'; i++) query[i] = proxy[i];
+                    query[i] = '\0';
+                    break;
+
+                case 'd':              /* int */
+                    query[i++] = 'd';
+                    query[i] = '\0';
+
+                    d = va_arg(ap, int);
+
+                    sprintf(proxy, query, d);
+
+                    for(i = 0; proxy[i] != '\0'; i++) query[i] = proxy[i];
+                    query[i] = '\0';
+                    printf("%s\n", query);
+                    break;
+
+                case 'c':              /* char */
+                    query[i++] = 'c';
+                    query[i] = '\0';
+
+                    c = (char) va_arg(ap, int);
+
+                    sprintf(proxy, query, c);
+
+                    for(i = 0; proxy[i] != '\0'; i++) query[i] = proxy[i];
+                    query[i] = '\0';
+                    break;
+             }
+
             ++count;
-            break;
-       }
-    
+        }
+    }
+
+
     va_end(ap);
-    
+
+
     if(count) rc = sqlite3_get_table(db, query, &result, &nrows, &ncols, &errmsg);
     else rc = sqlite3_get_table(db, sql, &result, &nrows, &ncols, &errmsg);
     

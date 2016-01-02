585f4f30601ef69b27a48094e8ae0b282eb6631a
Sergey <glavposhtamt@bk.ru>
2016-01-02 13:57:20 +0300

    Edit group-block

--- group.c
+++ group.c
@@ -2,11 +2,21 @@
 
 void initTables(sqlite3 * db, fC callback){
 
-  char * group = "CREATE TABLE IF NOT EXISTS GROUPTASK (" \
-                    "ID       INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL," \
-                    "TASKID   INTEGER          NOT NULL);";
+  system("./ttr");
 
-   sqlQuery(db, callback, group);
+  char * glist = "CREATE TABLE IF NOT EXISTS GROUPLIST (" \
+                    "ID       INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL," \
+                    "MESSAGE  CHAR(100)        NOT NULL,"\
+                    "DISPLAY  INTEGER          NOT NULL)";
+
+
+  char * gtask = "CREATE TABLE IF NOT EXISTS GROUPTASK (" \
+                    "ID       INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL," \
+                    "TIMEID   INTEGER          NOT NULL,"\
+                    "FOREIGN KEY(TIMEID) REFERENCES TIME(ID));";
+
+   sqlQuery(db, callback, glist);
+   sqlQuery(db, callback, gtask);
    foreignKey(db, 1);
 
 }
--- group.h
+++ group.h
@@ -2,6 +2,7 @@
 #define GROUP
 
 #include <stdio.h>
+#include <stdlib.h>
 
 #include "wrapper.h"
 
